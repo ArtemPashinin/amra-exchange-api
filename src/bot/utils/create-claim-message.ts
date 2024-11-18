7595a9757f1d5e5dabe6ced1425d7b3223b87b31
Artem <artem@MacBook-Air-Artem.local>
2024-11-18 19:23:24 +0300

    cors added

--- src/bot/utils/create-claim-message.ts
+++ src/bot/utils/create-claim-message.ts
@@ -1,7 +1,7 @@
 import { ICreateClaimDto } from 'src/financial/interfaces/dto/create-claim.dto';
 
 export function createClaimMessage(data: ICreateClaimDto): string {
-  return `Заявка ${data.sourceFinancial.financialName} -> ${data.targetFinancial.financialName}:
+  return `Заявка ${data.sourceFinancial.financialName} ➡️ ${data.targetFinancial.financialName}:
 Сумма ${data.sourceFinancial.financialName}: ${data.sourceFinancial.amount}
 Сумма ${data.targetFinancial.financialName}: ${data.targetFinancial.amount}
 Время: ${new Date()}`;
